c87fa1251ac20b135def4eeb0d18b27f87a3903e
Arif Lukmanul Hakim <2006079@itg.ac.id>
2024-12-21 08:45:12 +0700

    upade dashboard admin

--- resources/views/scr/admin/admin-dashboard.blade.php
+++ resources/views/scr/admin/admin-dashboard.blade.php
@@ -38,7 +38,7 @@
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                                         Total Siswa</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">250</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ \App\Models\ManajeSiswa::count() }}</div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-user-graduate fa-2x text-gray-300"></i>
@@ -55,7 +55,7 @@
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
                                         Mata Pelajaran</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">15</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ \App\Models\Mapel::count() }}</div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-book fa-2x text-gray-300"></i>
